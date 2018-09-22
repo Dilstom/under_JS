ea6356fb3581f98b999f3e5c9a31560ef2e5a21c
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-22 06:28:20 -0700

    functional pr using bind

--- functional_pr.js
+++ functional_pr.js
@@ -8,10 +8,36 @@ function mapEach(arr, cb) {
 
 var arr1 = [3, 4, 5]
 
-function multiplyByTwo(n) {
-    return n * 2
+// mapEach(arr1, function (n) {
+//     return n * 2
+// })
+// mapEach(arr1, function (n) {
+//     return n > 4
+// })
+
+// var checkLimit = function (limiter, n) {
+//     return n > limiter
+// }
+// mapEach(arr1, checkLimit.bind(this, 4))
+// to pass more than one argument, as cb() accepts only one
+
+var checkLimitSimplified = function (limiter) {
+    return function (limiter, n) {
+        return n > limiter
+    }.bind(this, limiter) // .bind(this, limiter, ... n)
 }
-function multiplyByThree(n) {
-    return n * 3
-}
-mapEach(arr1, multiplyByTwo)
+
+var arrLimited = mapEach(arr1, checkLimitSimplified(3))
+console.log(arrLimited)
+
+
+
+
+// function multiplyByTwo(n) {
+//     return n * 2
+// }
+// function multiplyByThree(n) {
+//     return n * 3
+// }
+// console.log('times by two: ', mapEach(arr1, multiplyByTwo))
+// console.log('times by three: ', mapEach(arr1, multiplyByThree))
