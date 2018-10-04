9673d862e8d9fea72e4b7e8850992f4c81c346d6
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-03 18:18:11 -0700

    es6 classes

---
+++ es6Classes.js
@@ -0,0 +1,25 @@
+// in JS 'class' keyword is still an object
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    greet() {
+        return 'Hi ' + this.firstname;
+    }
+}
+
+var john = new Person('John', 'Doe')
+
+// set the Prototype:
+// 'extends' sets the Prototype (__proto__) 
+class AnotherPerson extends Person {
+    constructor(firstname, lastname) {
+        super(firstname, lastname);
+    }
+
+    greet() {
+        return 'Yo ' + firstname;
+    }
+}
