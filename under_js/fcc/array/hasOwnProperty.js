87c88e125c4138178e40830fee7f6265ab9e631b
Dil Sombayeva <dilyasom@gmail.com>
2019-07-10 15:51:02 -0700

    hasOwnProperty() or `in` keyword

---
+++ under_js/fcc/array/hasOwnProperty.js
@@ -0,0 +1,33 @@
+let users = {
+ Alan: {
+  age: 27,
+  online: true,
+ },
+ Jeff: {
+  age: 32,
+  online: true,
+ },
+ Sarah: {
+  age: 48,
+  online: true,
+ },
+ Ryan: {
+  age: 19,
+  online: true,
+ },
+};
+
+function isEveryoneHere(obj) {
+ // change code below this line
+ return obj.hasOwnProperty('Alan', 'Jeff', 'Sarah', 'Ryan') ? true : false;
+ // if (obj.hasOwnProperty('Alan','Jeff','Sarah','Ryan')){
+ //   return true;
+ // } else {
+ //   return false;
+ // }
+ // change code above this line
+}
+
+console.log(isEveryoneHere(users));
+
+// or use `in` keyword: 'Alan' in obj;
