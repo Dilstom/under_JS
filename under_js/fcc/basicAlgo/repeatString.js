26b09e94430b64d4a56683c419e3c16de3738944
Dil Sombayeva <dilyasom@gmail.com>
2019-07-11 08:16:30 -0700

    repeat a string (recursion)

--- under_js/fcc/basicAlgo/repeatString.js
+++ under_js/fcc/basicAlgo/repeatString.js
@@ -9,7 +9,27 @@ function repeatStringNumTimes(str, num) {
  }
  return newStr;
 }
-
 repeatStringNumTimes('abc', 3);
 
 // abcabcabc
+
+// recursion
+
+function repeatStringNumTimes(str, num) {
+ // repeat after me
+ if (num < 0) {
+  return '';
+ }
+ if (num === 1) {
+  return str;
+ }
+ return str + repeatStringNumTimes(str, num - 1);
+}
+repeatStringNumTimes('abc', 3);
+
+// repeat()
+
+function repeatStringNumTimes(str, num) {
+ return str.repeat(num);
+}
+repeatStringNumTimes('abc', 3);
