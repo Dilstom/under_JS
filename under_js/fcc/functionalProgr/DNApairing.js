743b03c4a08ec41a8421b79cc708d76d0535cf14
Dil Sombayeva <dilyasom@gmail.com>
2019-08-07 11:29:47 -0700

    DNA pairing

---
+++ under_js/fcc/functionalProgr/DNApairing.js
@@ -0,0 +1,28 @@
+// The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+// Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+// Return the provided character as the first element in each array.
+
+// For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+// The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+
+function pairElement(str) {
+ const newArr = [];
+
+ for (let i = 0; i < str.length; i++) {
+  if (str[i] === 'G') {
+   newArr.push(['G', 'C']);
+  } else if (str[i] === 'C') {
+   newArr.push(['C', 'G']);
+  } else if (str[i] === 'T') {
+   newArr.push(['T', 'A']);
+  } else if (str[i] === 'A') {
+   newArr.push(['A', 'T']);
+  }
+ }
+ return newArr;
+}
+
+pairElement('GCG');
